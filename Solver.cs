723be12a4adc8d3c04d6f6bdc9e3723fb338e209
fortunato-sply <lucasfortunato2530@gmail.com>
2023-10-11 15:38:56 -0300

    fix: recursive search and solution way

--- Solver.cs
+++ Solver.cs
@@ -12,13 +12,35 @@ public class Solver
         solve(initial);
     }
 
-    private void solve(Space space)
+    private bool solve(Space space)
     {
         space.Visited = true;
 
-        if(space.IsSolution)
-            return;
+        if(space.Exit) {
+            space.IsSolution = true;
+            return true;
+        }
+
+        if(space.Left != null && !space.Left.Visited)
+            space.IsSolution = solve(space.Left);
+            if(space.IsSolution)
+                return true;
+
+        if(space.Top != null && !space.Top.Visited)
+            space.IsSolution = solve(space.Top);
+            if(space.IsSolution)
+                return true;
         
-        if(space.)
+        if(space.Bottom != null && !space.Bottom.Visited) 
+            space.IsSolution = solve(space.Bottom);
+            if(space.IsSolution)
+                return true;
+        
+        if(space.Right != null && !space.Right.Visited)
+            space.IsSolution = solve(space.Right);
+            if(space.IsSolution)
+                return true;
+        
+        return false;
     }
 }
